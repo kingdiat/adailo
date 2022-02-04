bc7724da9bcbb537733cfb252e56ebf128194304
kingdiat <kingdiat@smail.cyc.edu.tw>
2022-02-04 11:32:56 +0800

    add one two files

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-express = require('express');
-app = express()
+var express = require('express');
+var app = express();
 
+app.use('/', express.static('pub'));
+app.get('/', function(req, res) {
+    if (req.query.one == 'a') {
+        res.send('<H1>Hello, Express.<H1>');
+    }
+    else {
+        res.send('others');
+    }
+});
+app.get('/:name', function(req, res) {
+    console.log('Request Type:', req.method);
+    if (req.params.name == 'one.html') {
+        res.send('one.html');
+    }
+    else if (req.params.name == 'a') {
+        res.send('AAA');
+    }
+
+    res.send(req.params.name)
+});
+
+app.listen(3000, function() {
+    console.log('Express started!');
+});
